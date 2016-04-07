6a75a77571067db86a0c3953948c321abf5d7d1c
eringrand <eringrand@gmail.com>
2016-04-07 19:48:05 -0400

    corrected load data

--- loaddata.py
+++ loaddata.py
@@ -1,7 +1,6 @@
-
 import sqlite3 as sqlite
 import pandas as pd
 import pandas.io.sql as psql
 
 f = open("data.csv")
-eval = pd.read_csv(f,sep='\t',header = None)
+eval = pd.read_csv(f,sep=',', header = 'infer', low_memory=False)
