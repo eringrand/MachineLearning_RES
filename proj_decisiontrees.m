9cc8626de58fc5416fabe8ab55d5f3b32f2fb181
sg2928 <sg2928@columbia.edu>
2016-04-12 22:33:44 -0400

    sankara

--- proj_decisiontrees.m
+++ proj_decisiontrees.m
@@ -1,20 +1,40 @@
-%%%%%%%%%%%%%%%%decisiontrees----------
-
-clear all; close all;clc;
-
-x=xlsread('proj_tr_data.xlsx');
-y=xlsread('proj_test_data.xlsx');
-
-[Noofsamp,Noof_feat]=size(x);
-data1=x(:,1:Noof_feat-1);
-labels1=x(:,Noof_feat);
-tree = ClassificationTree.fit(data1,labels1);
-pred = predict(tree,y) ;
-%err=length(find(labels1~=pred))/length(labels1);
-
-n=(1:length(pred))';
-A=[n pred];
-
-filename = 'proj_test_labels.csv';
-csvwrite(filename,{'Id', 'Prediction'});
-csvwrite(filename,A,2,0);
+%%%%%%%%%%%%%%%%decisiontrees----------
+
+
+
+clear all; close all;clc;
+
+
+
+x=xlsread('proj_tr_data2.xlsx');
+
+y=xlsread('proj_test_data2.xlsx');
+
+
+
+[Noofsamp,Noof_feat]=size(x);
+
+data1=x(:,1:Noof_feat-1);
+
+labels1=x(:,Noof_feat);
+
+tree = ClassificationTree.fit(data1,labels1);
+
+pred = predict(tree,y) ;
+
+%err=length(find(labels1~=pred))/length(labels1);
+
+
+
+n=(1:length(pred))';
+
+A=[n pred];
+
+
+
+filename = 'proj_test_labels2.csv';
+
+csvwrite(filename,{'Id', 'Prediction'});
+
+csvwrite(filename,A,2,0);
+
--- project.m
+++ project.m
@@ -5,28 +5,28 @@
 clear all; close all;clc;
 %% [num,txt,raw] = xlsread(___) additionally returns the text fields in cell array txt, 
 %% and both numeric and text data in cell array raw
-[n,t,r]=xlsread('data.csv');
+[n,t,r]=xlsread('data2.csv');
 [R,C]=size(r(:,:));
 %%%%consider from the row the raw data
 n_1=n(2:R,:);t_1=t(2:R,:);r_1=r(2:R,:);
 A=cat2num(r_1);
 
-filename = 'proj_tr_data.xlsx';
+filename = 'proj_tr_data2.xlsx';
 
 
 
 % 
-xlswrite(filename,A);
+%xlswrite(filename,A);
+%
+%clear all;close all;clc;
+%[n,t,r]=xlsread('quiz.csv');
+%[R,C]=size(r(:,:));
+%%n_1=n(2:R,:);
+%%t_1=t(2:R,:);
+%r_1=r(2:R,:);
+%A=cat2num(r_1);
 
-clear all;close all;clc;
-[n,t,r]=xlsread('quiz.csv');
-[R,C]=size(r(:,:));
-%n_1=n(2:R,:);
-%t_1=t(2:R,:);
-r_1=r(2:R,:);
-A=cat2num(r_1);
-
-filename = 'proj_test_data.xlsx';
+%filename = 'proj_test_data.xlsx';
 
 
 
